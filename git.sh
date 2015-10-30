a5477e60118a231b7331b966e77f03b0798274ec
JinfengChen <jinfeng7chen@gmail.com>
2015-10-30 16:55:14 -0700

    update

--- git.sh
+++ git.sh
@@ -5,3 +5,8 @@ git remote add origin https://github.com/JinfengChen/RIL_update.git
 git push -u origin master
 
 
+echo "update"
+git add README.md achieve.list *.txt *.py *.sh
+git commit -m "update"
+git remote add origin https://github.com/JinfengChen/RIL_update.git
+git push -u origin master
